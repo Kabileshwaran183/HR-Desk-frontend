308e62e7c7c3603e907b77853995c6abd351f8f3
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-03-13 18:01:05 +0530

    change the local host to backend link

--- src/Component/Jobs/Jobapplication.jsx
+++ src/Component/Jobs/Jobapplication.jsx
@@ -57,7 +57,7 @@ const JobApplication = () => {
     };
 
     try {
-        const response = await fetch("http://localhost:5000/api/jobapplications", { 
+        const response = await fetch("https://hr-desk-backend.onrender.com/api/jobapplications", { 
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(applicationData)
